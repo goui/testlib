f4ab0f99c94eee1684116115ff64538d737cff91
Guillaume <guillaume.galliano@gmail.com>
2019-01-21 15:32:44 -0500

    Updated example app to use the library

--- example/app/src/main/java/com/github/goui/example/MainActivity.kt
+++ example/app/src/main/java/com/github/goui/example/MainActivity.kt
@@ -2,11 +2,14 @@ package com.github.goui.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.goui.mylibrary.LibActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main_text_view.setOnClickListener { startActivity(LibActivity.getStartingIntent(this)) }
     }
 }
